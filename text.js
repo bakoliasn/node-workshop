7d8215d08002b785207a5721cc568d3cbe758c55
bakoliasn <bakoliasn@gmail.com>
2015-11-17 21:38:35 +0000

    number gmame in node!!!

---
+++ text.js
@@ -0,0 +1,24 @@
+var prompt = require('prompt');
+var number = Math.round(10 * Math.random()).toString();
+var array = [];
+
+function random() {
+    prompt.get(['guess'], function(err, result) {
+        if (err) {
+            return console.log("THERE WAS AN ERROR... RUNNNNN");
+        }
+        console.log(typeof result.guess);
+        array.push(result.guess);
+        if (result.guess === number) {
+            return console.log(n=umber + " is the right guess!");
+        }
+        else if (array.length == 4) {
+            return console.log("WRONG... that was your last try, you guessed " + array + " but the answer was " + number);
+        }
+        else {
+            console.log("Wrong answer guess again! you have " + (4 - array.length) + " tries left!");
+            return random();
+        }
+    });
+}
+random();
